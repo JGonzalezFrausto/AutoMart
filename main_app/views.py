58aca567a1143f030f47201e34c3fd7b47788ed5
Javier González <j.gonzalez.frausto@gmail.com>
2019-09-25 18:50:50 -0500

    "usando datos dinamiso"

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    nombre = "VW Jetta"
+    modelo = 2018
+    precio = 153000
+    color = "Gris"
 
+    dict = { 'auto_nombre' :  nombre , 'auto_modelo' : modelo , 'auto_precio' : precio , 'auto_color' : color
+ 
+    }
     
-    
+    return render( request, 'index.html', dict)
+
