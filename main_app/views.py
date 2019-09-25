5d4ac481ee47f2352f9589a18af8e3b4decbeb51
Javier González <j.gonzalez.frausto@gmail.com>
2019-09-25 18:59:05 -0500

    usas datos dinamicos desde view

--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 def index(request):
     nombre = "VW Jetta"
     modelo = 2018
-    precio = 153000
+    precio = currency_converter(153000)
     color = "Gris"
 
     dict = { 'auto_nombre' :  nombre , 'auto_modelo' : modelo , 'auto_precio' : precio , 'auto_color' : color
