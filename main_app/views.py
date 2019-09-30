9ae22a295249e2de58bb57a7041634938c49b830
Javier González <j.gonzalez.frausto@gmail.com>
2019-09-30 18:55:03 -0500

    se intentó agregar imagen de autos

--- main_app/views.py
+++ main_app/views.py
@@ -6,17 +6,18 @@ def index(request):
     return render( request, 'index.html', {'autos' : autos})
 
 class Auto:
-    def __init__ (self, nombre, modelo , precio , color):
+    def __init__ (self, nombre, modelo , precio , color, img_url):
         self.nombre = nombre
         self.modelo = modelo
         self.precio = precio
         self.color = color
+        self.img_url = img_url
 
+default_image ="https://tiaecuador.vteximg.com.br/arquivos/ids/160501-1000-1000/192156000.jpg" 
 autos = [ 
-    Auto("VW Jetta", 2018 , 125000 , "Gris"),
-    Auto("Lexus", 2018 , 256000 , "Rojo"),
-    Auto("Futura", 1954 , 0 , "Aqua"),
-    Auto("Porsche", 2010 , 250000 , "Azul"),
-    Auto("VW GOL", 2018 , 175000 , "Azul")
-
-]
+    Auto("VW Jetta", 2018 , 125000 , "Gris", default_image), 
+    Auto("Lexus", 2018 , 256000 , "Rojo", default_image ), 
+    Auto("Futura", 1954 , 0 , "Aqua", default_image ), 
+    Auto("Porsche", 2010 , 250000 , "Azul", default_image ) ,
+    Auto("VW GOL", 2018 , 175000 , "Azul", default_image ),
+]   
